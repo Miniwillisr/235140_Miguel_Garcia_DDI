cf15c56f1fa3822252f9383d80073e69808c38a2
Miniwillisr <al235140@alumnos.uacj.mx>
2026-02-09 12:39:57 -0700

    Se Actualiz Practica Parallax

--- Parcial1/Practica_Parallax/js/script.js
+++ Parcial1/Practica_Parallax/js/script.js
@@ -2,11 +2,14 @@ const elemento1 = document.querySelector("#ele-1");
 const elemento2 = document.querySelector("#ele-2");
 const elemento3 = document.querySelector("#ele-3");
 const elemento4 = document.querySelector("#ele-4");
+const elemento5 = document.querySelector("#ele-5");
+
 
 const boton1 = document.getElementById("btn1"); // El # solo se usa en QuerySelector, no en getElementById
 const boton2 = document.getElementById("btn2");
 const boton3 = document.getElementById("btn3");
 const boton4 = document.getElementById("btn4");
+const boton5 = document.getElementById("btn5");
 const texto3= document.getElementById("texto3");
 
 let bandera=false;
@@ -28,12 +31,17 @@ boton1.addEventListener('click', ()=>{
 });
 
 //Seccion 2
-function cambiarSize(){ //Uso de Triada
-    elemento2.style.transform = grande ? "scale(1)" : "scale(1.1)";
-    elemento2.style.transition = "transform 0.3s";
-    grande = !grande;
+function cambiarSize(){ 
+        elemento2.addEventListener('mouseenter', () => {
+        elemento2.style.transform = "scale(1.1)";
+        elemento2.style.transition = "transform 0.3s";
+    });
+
+    elemento2.addEventListener('mouseleave', () => {
+        elemento2.style.transform = "scale(1)";
+    });
 }
-boton2.addEventListener('click', cambiarSize);
+cambiarSize();
 
 //Seccion 3
 function cambiarTexto(){
@@ -45,7 +53,12 @@ function cambiarTexto(){
     textoOG = !textoOG;
 }
 boton3.addEventListener('click', cambiarTexto);
+
 //Seccion 4
+function efectoSombra(){
+    
+}
+
 function colorRandom(){
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
