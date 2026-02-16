8d8cd2a6df0079cdac09cd03df5480034651d32f
Miniwillisr <al235140@alumnos.uacj.mx>
2026-02-16 11:53:20 -0700

    Avance en Ejemplo de Formularo Dinamico

--- Parcial1/Formulario_Dinamico/js/script.js
+++ Parcial1/Formulario_Dinamico/js/script.js
@@ -11,6 +11,21 @@ botonConfi.addEventListener("click",(e)=>{
     document.body.appendChild(nombre_info);  //muestra la informacion
 });
 
+function cambiarNumero(event){
+        console.log(event.target.value);
+        const contenido = document.querySelector("#contenedor_correos");
+        contenido.innerHTML = ""; //Limpia la etiqueta antes de empezar
+        
+        for(let i =1; i <= event.target.value; i++){
+            //Se agrega contenido usando la insecion de html por medio del Innerhtml, que agrega todo lo que esta dentro de htmlAgregar
+            //Este metodo reemplaza todo lo que esta dentro de la etiquta por lo nuevo
+            const htmlAgregar= `<label for="correo-${i}">Ingrese El Correo ${i}</label>
+            <input type="email" name="correo-${i}" id="correo-${i}">
+            <br>`;
+            contenido.innerHTML += htmlAgregar;
+        }
+}
+
 /*nombre.addEventListener("change", (e)=>{
     console.log(e.target.value);
     const usuario= new Usuario(e.target.value);
