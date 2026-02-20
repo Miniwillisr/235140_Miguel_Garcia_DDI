5ba32f6ca8d2fa18fa30e24eb84a025e6af23dd0
Miniwillisr <al235140@alumnos.uacj.mx>
2026-02-20 12:47:32 -0700

    Avance Formularios

--- Parcial2/Actividad en Clase/Formulario_Dinamico/js/script.js
+++ Parcial2/Actividad en Clase/Formulario_Dinamico/js/script.js
@@ -7,91 +7,105 @@ const Usuarios = [{ //1
     apellido: "Castro ",
     correo: "maria.castro@example.com",
     contraseña: "contraseña123",
-    },                                                                       
-    {//2
+},
+{//2
     nombre: "Amancio ",
     apellido: "Torres ",
     correo: "amancio.torres@example.com",
     contraseña: "contraseña456",
-    },
-    {//3
+},
+{//3
     nombre: "Miguel ",
     apellido: "Garcia ",
     correo: "miguel.garcia@example.com",
     contraseña: "contraseña789",
-    },
-    {//4
+},
+{//4
     nombre: "Luis ",
     apellido: "Peña ",
     correo: "luis.pena@example.com",
     contraseña: "contraseña101112",
-    },
-    {//5
+},
+{//5
     nombre: "Jesus ",
     apellido: "Gonzalez ",
     correo: "jesus.gonzalez@example.com",
     contraseña: "contraseña131415",
-    },
-    {//6
+},
+{//6
     nombre: "Ana ",
     apellido: "Hernandez ",
     correo: "ana.hernandez@example.com",
     contraseña: "contraseña161718",
-    },
-    {//7
+},
+{//7
     nombre: "Pepe ",
     apellido: "Torres ",
     correo: "pepe.torres@example.com",
     contraseña: "contraseña192021",
-    },
-     {//8
+},
+{//8
     nombre: "Benito ",
     apellido: "Juarez ",
     correo: "benito.juarez@example.com",
     contraseña: "contraseña222324",
-    },
-    {//9
+},
+{//9
     nombre: "Luis ",
     apellido: "Serna ",
     correo: "luis.serna@example.com",
     contraseña: "contraseña252627",
-    },
-    {//10
+},
+{//10
     nombre: "Roberto ",
     apellido: "Lopez ",
     correo: "roberto.lopez@example.com",
     contraseña: "contraseña282930",
-    }
-]; 
+}
+];
 
-botonConfi.addEventListener("click",(e)=>{
+botonConfi.addEventListener("click", (e) => {
     e.preventDefault();
-    const usuario= new Usuario(nombre.value,apellido.value);
+
+    const nuevoNombre = document.querySelector("#input-txt-nombre");
+    const nuevoApellido = document.querySelector("#input-txt-apellido");
+    const nuevoCorreo = document.querySelector("#input-correo");
+    const nuevaContra = document.querySelector("#input-contraseña");
+
+    const usuario = new Usuario(nombre.value, apellido.value);
     console.log(usuario);
     const nombre_info = document.createElement("h2"); //Metodo que crea elementos
     nombre_info.textContent = usuario.nombre;
     document.body.appendChild(nombre_info);  //muestra la informacion
 
     guardarDatos(usuario);
-    guardarDatos= ()=>{
+    guardarDatos = () => {
         console.log("Guardando Datos...");
     };
     guardarDatos();
+
+    Usuarios.push({
+        nombre: nuevoNombre.value,
+        apellido: nuevoApellido.value,
+        correo: nuevoCorreo.value,
+        contraseña: nuevaContra.value
+    });
+    console.log(Usuarios);
 });
 
-function cambiarNumero(event){
-        console.log(event.target.value);
-        const contenido = document.querySelector("#contenedor_correos");
-        contenido.innerHTML = ""; //Limpia la etiqueta antes de empezar
-        
-        for(let i =1; i <= event.target.value; i++){
-            //Se agrega contenido usando la insecion de html por medio del Innerhtml, que agrega todo lo que esta dentro de htmlAgregar
-            //Este metodo reemplaza todo lo que esta dentro de la etiquta por lo nuevo
-            const htmlAgregar= `<label for="correo-${i}">Ingrese El Correo ${i}</label>
+function cambiarNumero(event) {
+    console.log(event.target.value);
+    const contenido = document.querySelector("#contenedor_correos");
+    contenido.innerHTML = ""; //Limpia la etiqueta antes de empezar
+
+    for (let i = 1; i <= event.target.value; i++) {
+        //Se agrega contenido usando la insecion de html por medio del Innerhtml, que agrega todo lo que esta dentro de htmlAgregar
+        //Este metodo reemplaza todo lo que esta dentro de la etiquta por lo nuevo
+        const htmlAgregar = `<label for="correo-${i}">Ingrese El Correo ${i}</label>
             <input type="email" name="correo-${i}" id="correo-${i}">
             <br>`;
-            contenido.innerHTML += htmlAgregar;
-        }
+        contenido.innerHTML += htmlAgregar;
+    }
 }
 
 /*nombre.addEventListener("change", (e)=>{
@@ -106,13 +120,13 @@ apellido.addEventListener("change", (e)=>{
 });
 */
 
-class Usuario{
-    constructor(nom, ape){
-        this.nombre= nom;
-        this.apellido= ape;
+class Usuario {
+    constructor(nom, ape) {
+        this.nombre = nom;
+        this.apellido = ape;
     }
 
-    mostrarDatos(){
+    mostrarDatos() {
         console.log(`Nombre: ${this.nombre} Apellido: ${this.apellido}`);
     }
 }
@@ -122,7 +136,7 @@ let usuario2 = { //aqui el profe puso Let
     nombre: "Juan",
     apellido: "Perez",
     Edad: 28,
-    mostrarDatos: function(){
+    mostrarDatos: function () {
         console.log(`Nombre: ${this.nombre} 
             Apellido: ${this.apellido} 
             Edad: ${this.Edad}`);
@@ -130,9 +144,56 @@ let usuario2 = { //aqui el profe puso Let
 }
 
 //Nos permite Guardar nombres dentro de una constante
-let guardarDatos = (usuario)=>{
+let guardarDatos = (usuario) => {
     usuario.mostrarDatos();
     usuario2.mostrarDatos();
     usuario2.nombre = "Nuevo Nombre";
     usuario2.mostrarDatos();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const contenedor_usuarios = document.querySelector("#contenedor_usuarios");
+
+    for (let i = 0; i < Usuarios.length; i++) {
+        const contenedor_usuario = document.createElement("div");
+        contenedor_usuario.id = "contenedor_usuario";
+
+        const nombre = document.createElement("label");
+        nombre.textContent = "Nombre: ";
+
+        const contenidoNombre = document.createElement("span");
+        contenidoNombre.textContent = Usuarios[i].nombre;
+
+        const apellido = document.createElement("label");
+        apellido.textContent = "Apellido: ";
+
+        const contenidoApellido = document.createElement("span");
+        contenidoApellido.textContent = Usuarios[i].apellido;
+
+        contenedor_usuario.onclick = () => {
+            //console.log(Usuarios[i].nombre);
+
+            const correo = document.createElement("input");
+            correo.placeholder = "Ingresa un correo"
+            correo.type = "email ";
+
+            const contraseña = document.createElement("input");
+            contraseña.placeholder = "Ingrese La Contraseña";
+            contraseña.type ="password";
+
+            contenedor_usuario.appendChild(correo);
+            contenedor_usuario.appendChild(contraseña);
+
+            console.log(Usuarios[i].nombre);
+            console.log(Usuarios[i].apellido);
+            console.log(Usuarios[i].correo);
+            console.log(Usuarios[i].contraseña);
+        }
+        contenedor_usuario.appendChild(nombre);
+        contenedor_usuario.appendChild(contenidoNombre);
+        contenedor_usuario.appendChild(apellido);
+        contenedor_usuario.appendChild(contenidoApellido);
+
+        contenedor_usuarios.appendChild(contenedor_usuario);
+    }
+});
