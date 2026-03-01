3cb24ede79a9c43b9550d558e1120c044bf5fa11
Miniwillisr <al235140@alumnos.uacj.mx>
2026-02-28 17:35:08 -0700

    Avanze en Fetch y Registro

--- Parcial2/Actividad en Clase/Fetch/JS/script.js
+++ Parcial2/Actividad en Clase/Fetch/JS/script.js
@@ -9,6 +9,8 @@ fetch(url).then( //Este fetch solo aplica para el placeholder
     }
 ).then(
     datos=> {
+        const contenedor = document.querySelector("#pokeCont");
+
         for(let i=0; i<datos.results.length; i++){
             console.log(datos.results[i].url);
 
@@ -18,11 +20,26 @@ fetch(url).then( //Este fetch solo aplica para el placeholder
                     return respuesta.json();
                 }
             }).then(datos2 => { // se cambia el nombre de datos para que no de conflicto
-                console.log(datos2.forms);
-                console.log(datos2.sprites.front_default);
-                console.log(datos2.name);
-                console.log(datos2.stats);
-                console.log(datos2.types);
+               //se crea la tarjeta y sus datos como en la practica anterior
+                const tarjeta = document.createElement("div");
+                tarjeta.classList.add("tarjeta-Pokemon");
+
+                const imagen = document.createElement("img");
+                imagen.src = datos2.sprites.front_default;
+                imagen.alt = `Imagen de ${datos2.name}`; //texto por si la imagen no carga
+
+                const nombre = document.createElement("h3");
+                nombre.textContent = datos2.name;
+
+                const info = document.createElement("p");
+                const tipos = datos2.types.map(typeInfo => typeInfo.type.name).join(", "); //Muestra todos los tipos y los une con una coma
+                //const tipos = datos2.types[0].type.name; solo muestra el primer tipo
+                info.textContent = `Tipo: ${tipos}`;
+
+                tarjeta.appendChild(imagen);
+                tarjeta.appendChild(nombre);
+                tarjeta.appendChild(info);
+                contenedor.appendChild(tarjeta);
             })
         }
     }
